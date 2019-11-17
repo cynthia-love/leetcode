3b1e5033da67e188036371bd0425148b0e60e883
Cynthia <bbliu666@126.com>
2019-11-17 16:04:52 +0800

    commit

--- lesson 0001-0100/lesson 0001-0010/0008-String to Integer/solution-2.py
+++ lesson 0001-0100/lesson 0001-0010/0008-String to Integer/solution-2.py
@@ -15,6 +15,9 @@ class Solution:
         flag = -1 if s[0] == '-' else 1
         s = s[1:] if s[0] in {'+', '-'} else s
 
+        # 仔细思考这里为什么要这么做
+        # 比如最大127, 那么不能算完再和127比, 因为即使大于127截断后还是在127内
+        # 所以只能计算前比, 比如32 5, 32大于12, 再乘以10肯定超限
         INT_MIN, INT_MAX = -2**31, 2**31-1
         p_min, q_min = int(INT_MIN/10), INT_MIN-int(INT_MIN/10)*10
         p_max, q_max = int(INT_MAX/10), INT_MAX-int(INT_MAX/10)*10
@@ -22,6 +25,7 @@ class Solution:
         ans = 0
         for i in s:
             try:
+                # 123aa, 枚举到a的时候会退出
                 q = int(i)
                 if ans < p_min or (ans == p_min and flag*q < q_min):
                     return INT_MIN
