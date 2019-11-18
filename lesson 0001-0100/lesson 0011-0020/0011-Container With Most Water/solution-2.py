cc676f09859bb0b2119da29c7a43c003babe4982
Cynthia <bbliu666@126.com>
2019-11-18 20:12:29 +0800

    'commit'

--- lesson 0001-0100/lesson 0011-0020/0011-Container With Most Water/solution-2.py
+++ lesson 0001-0100/lesson 0011-0020/0011-Container With Most Water/solution-2.py
@@ -3,29 +3,32 @@
 # Author: Cynthia
 
 """
-    方法2, 双指针
-    受方法1启发, 左边界右移必须找大于原高度的, 其实右边界也一样, 一次遍历O(n)
-    每次移动短的那个(移动长的容量只会减不会增)
+    方法2, 双指针, 实质就是在移动的过程中不断消去不可能成为最大值的状态
+
+    受方法1启发, 左边界右移必须找大于原高度的才有可能容量增加, 其实右边界也一样
+    leetcode上给的解法是每次移动1, 之后算面积, 再比较左右, 短的那侧再移动1
+
+    感觉这里是可以继续优化的, 没必要每次移动1造成很多不必要的计算ans
+    比如左侧之前最高8, 那么右移得找到比这个8大的才有意义, 不然肯定不如选8这个边界
+    稍微快了一点, 效果有限, 毕竟都是O(n)
 """
 from typing import List
 
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l, r, ans, ml, mr = 0, len(height)-1, 0, height[0], height[len(height)-1]
+        l, r, ans, lmax, rmax = 0, len(height)-1, 0, 0, 0
 
         while l < r:
-            print(l, r)
             ans = max(ans, min(height[l], height[r])*(r-l))
-            if height[l] <= height[r]:
-                while l+1 < r and height[l+1] <= ml:
-                    l += 1
-                ml = height[l]
-            else:
-                while r-1 > l and height[r-1] <= mr:
-                    r -= 1
-                mr = height[r]
+            lmax, rmax = max(lmax, height[l]), max(rmax, height[r])
 
+            if height[l] < height[r]:
+                while l < r and height[l] <= lmax:
+                    l += 1
+            else:
+                while r > l and height[r] <= rmax:
+                    r -= 1
 
         return ans
 
