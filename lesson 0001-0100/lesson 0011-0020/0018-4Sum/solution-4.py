0ce5a85c4018426ff52087c4212c9d7d82b787b9
Cynthia <bbliu666@126.com>
2019-11-21 00:00:09 +0800

    commit

--- lesson 0001-0100/lesson 0011-0020/0018-4Sum/solution-4.py
+++ lesson 0001-0100/lesson 0011-0020/0018-4Sum/solution-4.py
@@ -3,6 +3,37 @@
 # Author: Cynthia
 
 """
-    方法3, k数和通用方法
+    方法3, k数和通用方法, 第一种思路, 一个一个减
 """
+from typing import List
+
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+        nums.sort()
+
+        def rf(nums, target, k):
+            print(nums)
+            ans = []
+
+            if k == 1:
+                return [[i] for i, v in enumerate(nums) if v == target]
+
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i-1]:
+                    continue
+
+                ts = rf(nums[i:], target-nums[i], k-1)
+                ans += [[i]+x for x in ts]
+
+
+            return ans
+
+        return rf(nums, target, 4)
+
+
+
+s = Solution()
+print(s.fourSum([1, 0, -1, 0, -2, 2], 0))
 
