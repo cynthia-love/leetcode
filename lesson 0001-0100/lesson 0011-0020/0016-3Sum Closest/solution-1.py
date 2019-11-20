744c259e9426ea25175eec2ed28437a1f4816f44
Cynthia <bbliu666@126.com>
2019-11-20 10:04:23 +0800

    commit

--- lesson 0001-0100/lesson 0011-0020/0016-3Sum Closest/solution-1.py
+++ lesson 0001-0100/lesson 0011-0020/0016-3Sum Closest/solution-1.py
@@ -7,6 +7,8 @@
     比如: [-1, 2, 1, -4]和1, 返回2; 假定结果唯一
     外层一个循环, 跳过重复遇到的值
     内层双指针, 也有优化空间, 如果左边两个加起来已经大于sum2, 退出内层循环; 右侧亦然
+    同时, 内层也可以像外层那样跳过重复值
+    好像除了外层循环, 其他优化效果有限...
 """
 from typing import List
 
@@ -36,17 +38,33 @@ class Solution:
                     ans = nums[i]+nums[r]+nums[r-1]
                     continue
 
+            hl, hr = set(), set()
+
             while l < r:
+
+                if nums[l] in hl:
+                    l += 1
+                    continue
+
+                if nums[r] in hr:
+                    r -= 1
+                    continue
+
                 if nums[l]+nums[r] == sum2:
                     return target
                 if nums[l]+nums[r] < sum2:
                     if sum2-(nums[l]+nums[r]) < abs(target-ans):
                         ans = nums[i]+nums[l]+nums[r]
+                    # 移动的时候才加, 而不是写到前面
+                    hl.add(nums[l])
                     l += 1
+
                 else:
                     if (nums[l]+nums[r])-sum2 < abs(target-ans):
                         ans = nums[i]+nums[l]+nums[r]
+                    hr.add(nums[r])
                     r -= 1
+
         return ans
 
 
