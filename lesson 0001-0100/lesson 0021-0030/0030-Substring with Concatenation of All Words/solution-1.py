f77ad343554ebd36fadc3fb84c320b06926d4d55
Cynthia <bbliu666@126.com>
2019-12-24 16:59:22 +0800

    commit

--- lesson 0001-0100/lesson 0021-0030/0030-Substring with Concatenation of All Words/solution-1.py
+++ lesson 0001-0100/lesson 0021-0030/0030-Substring with Concatenation of All Words/solution-1.py
@@ -7,51 +7,49 @@
     找到s中所有子串的起始索引, 子串的概念为words中的所有单词拼起来(不限先后次序)
     比如这里可以找foobar也可以找barfoo, 限定每个单词长度一致
 """
-import re
 from typing import List
 
 
 class Solution:
-    def findSubstring2(self, s: str, words: List[str]) -> List[int]:
-        if not s or not words:
-            return []
-        size_l, size_w = len(words), len(words[0]),
-        l_index, ans = [0] * len(words), []
-
-        for o in range(len(s)-size_l * size_w+1):
-            l_index = [0] * len(words)
-            for i in range(o, o+size_l * size_w):
-                flag = False
-                for j in range(size_l):
-                    if l_index[j] > size_w - 1:
-                        continue
-
-                    if words[j][l_index[j]] == s[i]:
-                        l_index[j] += 1
-                        flag = True
-
-                    if l_index[j] > size_w - 1:
-                        l_index = [x if x > size_w - 1 else 0 for x in l_index]
-
-                if not flag:
-                    l_index = [0] * len(words)
-
-                if sum(l_index) == size_l * size_w:
-                    ans.append(o)
-                    l_index = [0] * len(words)
-
-        return ans
-
 
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if not s or not words: return []
 
-        ans, index, lens, size = [], 0, len(words), len(words[0])
+        ans, lens, size, index = [], len(words), len(words[0]), 0
+
+        def isMatch(subs, subwords):
+            lens, size = len(subwords), len(subwords[0])
+            pos, l_pos = 0, [0] * lens
+
+            while pos <= index + lens * size - 1:
+
+                for i in range(lens):
+                    if l_pos[i] > size - 1: continue
+                    if words[i][l_pos[i]] == s[pos]:
+                        l_pos[i] = l_pos[i] + 1
+                        if l_pos[i] > size - 1:
+                            l_pos = [x if x > size - 1 else 0 for x in l_pos]
+                            break
+                pos += 1
+
+            if sum(l_pos) == lens * size:
 
         while index <= len(s)-lens*size:
 
+            for item in ans:
+                if index-item < lens*size and (index-item) % size == 0:
+                    print(item)
+                    # 这里优化
+                    break
 
+
+                ans.append(index)
+                match = True
+
+            index += 1
+
+        return ans
 
 
 s = Solution()
-print(s.findSubstring("", []))
+print(s.findSubstring("aaaaaaaa", ["aa","aa", "aa"]))
