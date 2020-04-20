76cd0662d0e522bd259a5860310bf3cc9c5dc3a1
Cynthia <bbliu666@126.com>
2020-04-20 15:29:01 +0800

    commit

---
+++ basic/chapter 01/solution-1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Author: Cynthia
+
+"""
+    第1章 就这么愉快地开始吧
+"""
+
+print("I love fishc.com")
+print(5+3)
+print(1234567890987654321*1234567890987654321)
+print("Well water " + "River")
+print("I love python\n"*3)
---
+++ basic/chapter 02/solution-1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Author: Cynthia
+
+"""
+    第2章  用Python设计第一个游戏
+"""
+
