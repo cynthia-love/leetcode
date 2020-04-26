9dfe04dfd0c0791e95e2631608e4bda49c455cad
Cynthia <bbliu666@126.com>
2020-04-26 20:24:49 +0800

    commit

--- basic/chapter 05/solution-3.py
+++ basic/chapter 05/solution-3.py
@@ -7,5 +7,76 @@
     更确切地讲, str更类似于tuple, 其内容不可改变
 """
 
-a = "abc"
+a = "I love fishc.com"
 print(a)
+print(a[0])
+print(a[1:3])
+print(a[1:8:2])
+print(a[::-1])
+b = a[:5]+"****"+a[5:]
+print(b)
+
+# 内置方法, 都是返回一个新的, 而不是在原字符串上改
+c = "aAAAAAAA BBB"
+print(c.capitalize())  # 注意是直接返回一个新的字符串, 大小写互换?
+print(c.casefold())  # 全部转换为小写
+print(c.center(18, "*"))  # 居中, 左右用*填充; 当width小于等于字符串长度时无变化
+print(c.count("A", 0, 3))  # start和end参数可不指定
+print(c.encode("utf-8"))  # 用指定的编码格式对字符串进行编码
+print(c.endswith("A BBB", 0, 13))  # start end可不指定
+print(c.expandtabs(tabsize=4))  # 把\t转换为空格, 意义不大, replace也可以实现
+print(c.find("A B", 0, 13))  # 找到了返回索引, 找不到返回-1
+print(c.index("A B", 0, 13))  # 找到了返回索引, 找不到抛出异常
+print(c.isalnum())  # 是否仅由字母和数字组成; 空字符串认为是False
+print(c.isalpha())  # 是否仅由字母组成
+print(c.isdecimal())  # 是否仅包含十进制数字
+print(c.isdigit(), c.isnumeric())  # 是否仅包含数字
+print(c.islower(), c.isupper())  # 是否全是小写字母
+print(c.isspace())  # 是否仅包含空格
+print("Hello World".istitle())  # 标题字符串指每个单词首字母大写
+print(c.join("*****"))  # 注意不要理解反了, 是前面的join后面的, *.join(ABC)变成A*B*C
+print(c.ljust(20))  # 左对齐, 不够的补0
+print(c.lower(), c.upper())  # 全部转换为小写
+print(c.lstrip())  # 去掉左边的所有空格
+print(c.partition("A B"))  # 用sub去分割字符串, 返回三元组, 前面的子串, sub, 后面的子串
+print(c.partition("C"))  # 没找到子串, 还是返回三元组, 原串, "", ""; partition相当于把find和切片操作合并成一步了
+print(c.replace(" ", "*", 3))  # count表示替换不超过count次, 可不指定
+print(c.rfind("B"))
+print(c.rindex("B"))
+print(c.rjust(30))  # 左边用空格补齐
+print(c.zfill(30))  # 左边用0补齐
+print(c.rpartition("A B"))  # 从右边找到第一个sub去切分
+print(c.rstrip("B"))  # strip除了可以去掉收尾空格, 也可以指定字符串去掉指定字符
+print("a b c d e f g".split(" ", 3))  # 3表示最多切分3次; ['a', 'b', 'c', 'd e f g']
+print("""
+aaa
+bbb
+ccc""".splitlines())  # 按\n去切分['', 'aaa', 'bbb', 'ccc']
+print(c.startswith("aA", 0, 8))
+print(c.strip())
+print(c.swapcase(), c.capitalize())  # capitalize有问题
+print(c.title())
+print(c.translate(str.maketrans("ab", "ba")))  # 多个单字符替换用replace会很麻烦, 用translate映射比较方便
+# 同一分隔符多次拼接, 用+很麻烦, 灵活使用join
+print("I" + " " + "like" + " " + "China")
+print(" ".join(["I", "like", "China"]))
+
+
+# 格式化
+# 两种方法, 一种类似c里的%f, %c啥的, 不好用; 推荐用format, format支持按位置索引和按变量名索引
+d = "{0}第一个插入位置{1}第二个插入位置{2}第三个插入位置".format("a", "b", "c")
+print(d)
+e = "{x}第一个变量{y}第二个变量{z}第三个变量".format(y="haha", x="heihei", z="hehe")
+print(e)
+f = "{0}加入格式控制{1:.2e}加入格式控制".format(3.1, 3.1)
+print(f)  # 3.1加入格式控制3.10e+00加入格式控制
+
+
+# 部分转义字符
+print("\'")  # 外层是双引号这里加\也是没问题的
+print("\"")
+print("aaa\tbbb\nccc")
+print("aaa\\bbb")
+print("\x32")
+
+
---
+++ basic/chapter 05/solution-4.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# Author: Cynthia
+
+"""
+    序列list, tuple, str统称为序列; list可变, tuple str不可变
+"""
+
+# list把一个可迭代对象转换为列表
+a = list()
+b = list("like")
+c = list((1, 2, 3, 4, 5))
+print(a, b, c)
+
+# tuple把一个可迭代对象转换为元组
+a = tuple()
+b = tuple("like")
+c = tuple([1, 2, 3, 4, 5])
+print(a, b, c)
+
+# str把一个对象转换为字符串
+a = str()  # ""
+b = str([1, 2, 3])  # [1, 2, 3]
+print(a, b)
+print(str(1), str(1.222))
+
+# len返回列表的长度
+print(len([1, 2, 3]))
+print(len((1, 2, 3)))
+print(len("abc"))
+
+# max/max
+print(max([1, 2, 3, -1, -2]))
+print(max("I like adjfk v zk dfad"))  # z
+print(max(1, 3, 8, 9))  # 注意1, 2, 8, 9可以理解为tuple的, ()不是必须的, 对于tuple来说, 关键的是, 而不是()
+# 注意, python里不存在char概念, 不能字符和数字直接比较
+# print(max(1, "a")), 这里会报错
+
+# sum, 序列元素必须是数字
+print(sum([1, 2, 3, 4, 5]))
+
+# sorted, 排序, 输出结果类型恒定list
+a = [1, 8, 9, 2]
+b = sorted(a, key=lambda x: x+1, reverse=True)
+print(b)
+print(sorted("aexxfeab"))  # 即使输入是str, 排序结果也会变成list
+
+# reversed, 输出结果类型恒定迭代器iterator
+a = [1, 8, 9, 2]
+b = reversed(a)
+print(list(b))  # 迭代器是可迭代对象, 所以可以直接强转成list
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 最关键的, enumerate, 将一个可迭代对象转换成一个二元组可迭代对象, 索引+元素
+a = ["a", "b", 8]
+b = "like"
+print(list(enumerate(a)))  # [(0, 'a'), (1, 'b'), (2, 8)]
+print(list(enumerate(b)))  # [(0, 'l'), (1, 'i'), (2, 'k'), (3, 'e')]
+# 也可以用for遍历
+for s in enumerate(a):
+    print(s[0], s[1])
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+# 最关键的, zip, 同索引压到一起, 可以理解为一个矩阵, 行优先转为列优先
+a = [1, 2, 3, 4, 5]
+b = "hello"
+c = (8, 100, 2)
+"""
+    1   2   3   4   5
+    h   e   l   l   o
+    8   100
+    结果是[(1, 'h', 8), (2, 'e', 100)]   (长度取最短的那个)
+"""
+print(list(zip(a, b, c)))  # 输出可迭代对象, 可用list强转
+for x in zip(a, b, c):
+    print(x)
