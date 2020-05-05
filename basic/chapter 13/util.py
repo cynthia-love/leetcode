853b117364849b84ed0a7262de1ee512145f1e5c
Cynthia <bbliu666@126.com>
2020-05-05 23:56:42 +0800

    commit

--- basic/chapter 13/util.py
+++ basic/chapter 13/util.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # Author: Cynthia
-
+"""
+    这里是帮助信息
+    会进到help函数的DESCRIPTION部分
+    已经自动赋给模块的__doc__属性
+"""
 x = 1
 def f():
+    """
+    这里是函数的帮助信息
+    """
     print("helloworld_同级目录")
 
 class C:
+    """
+    这里是类的帮助信息
+    """
     @classmethod
     def cm(cls):
         print("cm_同级目录")
