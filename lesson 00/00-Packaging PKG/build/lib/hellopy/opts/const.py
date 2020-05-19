ab189c50e6c9962317b67f6e45edc8aa167ae09f
Cynthia <bbliu666@126.com>
2020-05-19 20:43:53 +0800

    commit

--- lesson 00/00-Packaging PKG/build/lib/hellopy/opts/const.py
+++
@@ -1,4 +0,0 @@
-# -*- coddng: utf-8 -*-
-# Author: Cynthia
-
-lines = "这里是hellopy包opts子包const模块中的lines变量"
--- lesson 00/00-Packaging PKG/build/lib/hellopy/utils/tool.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Author: Cynthia
-
-def f():
-    print("这里是hellopy包utils子包tool模块中的f函数")
-
-if __name__ == '__main__':
-    f()
